9cd3e71f7bbcdace96aadd227a7c60ebc00c4c13
Lex_TM <BenuelBertran@gmail.com>
2019-01-10 11:42:11 +0300

    add to github

---
+++ build/precache-manifest.3bdcbdeb8e40a99e88c6f66035585a5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e1e1c2273a7d0e5d176",
+    "url": "/comments/static/js/runtime~main.9e1e1c22.js"
+  },
+  {
+    "revision": "003a667b090320bcdf76",
+    "url": "/comments/static/js/main.003a667b.chunk.js"
+  },
+  {
+    "revision": "07220eb69638ac74c097",
+    "url": "/comments/static/js/1.07220eb6.chunk.js"
+  },
+  {
+    "revision": "003a667b090320bcdf76",
+    "url": "/comments/static/css/main.910e1016.chunk.css"
+  },
+  {
+    "revision": "4a5210857013d018ab08f0ce0f250fff",
+    "url": "/comments/index.html"
+  }
+];
